199feb67993979854b2a0f1b0f944d2bf2a7f344
Martina Kopecká <martina.kopecka@juicymo.cz>
2020-05-03 20:03:04 +0200

    Updates

---
+++ precache-manifest.9907e656c7e9672a600dc398372909a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7fb34c31057ae267945f6f45c1ae6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dd7bd135d590f77e27f",
+    "url": "/static/css/main.f889852f.chunk.css"
+  },
+  {
+    "revision": "e492912667fff79f9cf2",
+    "url": "/static/js/2.ef92eb93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ef92eb93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dd7bd135d590f77e27f",
+    "url": "/static/js/main.305da2c2.chunk.js"
+  },
+  {
+    "revision": "27c1245d2b13ff47af1e",
+    "url": "/static/js/runtime-main.96ab796f.js"
+  }
+]);
