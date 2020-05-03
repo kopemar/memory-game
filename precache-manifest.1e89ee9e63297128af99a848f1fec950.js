dc553b1f15fa9dc02981c6ef4fcee973c3772a6c
Martina Kopecká <xykopecka@gmail.com>
2020-05-03 20:39:19 +0200

    Updates

---
+++ precache-manifest.1e89ee9e63297128af99a848f1fec950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292ce60a097eacac6f7a260c3baef280",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "6fcb08db58166af3fde5",
+    "url": "/memory-game/static/css/main.f889852f.chunk.css"
+  },
+  {
+    "revision": "d9de0e9d8ac775e9aee5",
+    "url": "/memory-game/static/js/2.1fa74ae5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memory-game/static/js/2.1fa74ae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fcb08db58166af3fde5",
+    "url": "/memory-game/static/js/main.913e8f78.chunk.js"
+  },
+  {
+    "revision": "a7295d09a3c7d39f3388",
+    "url": "/memory-game/static/js/runtime-main.cd8959cd.js"
+  }
+]);
